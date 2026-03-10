e00d18f7eafd4705d366ecce4f76f56e8dda1539
ri5hii <rishi.0w0@gmail.com>
2026-03-11 01:06:29 +0530

    implemented basic engine interface

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -1,1 +1,38 @@
 package engine
+
+import (
+	"log/slog"
+	"sync"
+	"sync/atomic"
+)
+
+const (
+	Uninitialized = "uninitialized"
+	Initialized   = "initialized"
+	Running       = "running"
+	Shutdown      = "shutdown"
+)
+
+type Engine struct {
+	logger    *slog.Logger
+	status    atomic.Value
+	closeOnce sync.Once
+}
+
+func New(log *slog.Logger) *Engine {
+	eng := &Engine{
+		logger: log,
+	}
+	eng.status.Store(Initialized)
+	return eng
+}
+
+func (eng *Engine) Start() {
+	eng.status.Store(Running)
+	eng.logger.Info("engine started: ", "status: ", Running)
+}
+
+func (eng *Engine) Shutdown() {
+	eng.status.Store(Shutdown)
+	eng.logger.Info("engine stopped: ", "status: ", Shutdown)
+}
